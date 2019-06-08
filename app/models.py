24fd5cc80cb59afcf790748e7f5da0a07d8bc6e1
lwairore <kwairore@gmail.com>
2019-06-08 10:15:46 +0300

    Create a class of USer that has id, username , email and password_hash as it's columns.

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String)
+    password_hash = db.Column(db.String(255))   
+
+
+    @property
+    def password(self):
+        raise AttributeError('You are not allowed to access password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return "Writer {}".format(self.username)
