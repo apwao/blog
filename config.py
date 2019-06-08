725572a07c6713c25095fdf2dd05dd4fbd26b931
lwairore <kwairore@gmail.com>
2019-06-08 10:10:15 +0300

    Create class Config, ProdConfig, DevConfig configurations

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://karangu:lmzongolo8754@localhost/blog3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
