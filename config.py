60cb8f50c188cc8f6f948db31b79ec196ccba798
lwairore <kwairore@gmail.com>
2019-06-10 19:44:09 +0300

    Add configurations for the quotes API Key, Markdown and Gmail

--- config.py
+++ config.py
@@ -3,6 +3,16 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://karangu:lmzongolo8754@localhost/blog3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    QUOTES_API_KEY = os.environ.get('QUOTES_API_KEY')
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    
 
 class ProdConfig(Config):
     pass
