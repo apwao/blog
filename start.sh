b0afb0bcaa5438ea0d661b2eac0bd074ada9526a
lwairore <kwairore@gmail.com>
2019-06-10 20:24:18 +0300

    Deployment to Heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY="yes we can....!!!"
+export SECRET_KEY="how now!"
 export QUOTES_API_KEY="http://quotes.stormconsultancy.co.uk/random.json"
 export MAIL_USERNAME="pasowayi@gmail.com"
 export MAIL_PASSWORD="40A79jkL3B2i"
