a65952b9618f77e4c5af5019288523599c0eea2a
lwairore <kwairore@gmail.com>
2019-06-10 20:37:53 +0300

    Remove passsword for gmail

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 export SECRET_KEY="how now!"
 export QUOTES_API_KEY="http://quotes.stormconsultancy.co.uk/random.json"
-export MAIL_USERNAME="pasowayi@gmail.com"
-export MAIL_PASSWORD="40A79jkL3B2i"
+
 
 python3.6 manage.py server
