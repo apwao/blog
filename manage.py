965547756aae482c11f8ac9015b05eaabef4f3c6
lwairore <kwairore@gmail.com>
2019-06-10 19:46:55 +0300

    Import Manager and Server from flask_script

--- manage.py
+++ manage.py
@@ -1,13 +1,23 @@
 from app import create_app,db
 from flask_script import Manager, Server
+
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
 manager = Manager(app)
-migrate = Migrate(app, db)
+Migrate = Migrate(app, db)
 
 manager.add_command('server', Server)
 manager.add_command('db', MigrateCommand)
 
-if __name__ == '__main__':
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+
+
+if __name__ == "__main__":
     manager.run()
