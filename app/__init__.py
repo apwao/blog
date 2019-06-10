2782b9aed8305906dc807d156036bae42e9ab3e1
lwairore <kwairore@gmail.com>
2019-06-10 19:48:28 +0300

    Add configurations for simpleMDE markdown

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_simplemde import SimpleMDE
+from flask_mail import Mail
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+simple = SimpleMDE()
+
+mail = Mail()
 
 login_manager = LoginManager()
 
@@ -20,8 +24,17 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    simple.init_app(app)
+    mail.init_app(app)
 
     from .auth import auth
-    app.register_blueprint(auth)     
+    app.register_blueprint(auth)
+    
+    from .user import user
+    app.register_blueprint(user)
+
+    from .requests import configure_request
+    configure_request(app)
 
     return app
+
